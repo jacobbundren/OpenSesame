d9eb4793e475334e0dcb36c60c743ab9694c74e1
Jacob Bundren <jacobbundren@Jacobs-MBP.attlocal.net>
2021-04-14 17:25:55 -0500

    initial layout

--- components/Keypad.js
+++ components/Keypad.js
@@ -1,8 +1,90 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 
 const Keypad = () => {
-  return <View style={{}}></View>;
+  return (
+    <View style={styles.keypadContainer}>
+      <View style={styles.keypadRow}>
+        <View style={styles.keypadButton}>
+          <Text style={styles.keypadNumber}>1</Text>
+        </View>
+        <View style={styles.keypadButton}>
+          <Text style={styles.keypadNumber}>2</Text>
+        </View>
+        <View style={styles.keypadButton}>
+          <Text style={styles.keypadNumber}>3</Text>
+        </View>
+      </View>
+      <View style={styles.keypadRow}>
+        <View style={styles.keypadButton}>
+          <Text style={styles.keypadNumber}>4</Text>
+        </View>
+        <View style={styles.keypadButton}>
+          <Text style={styles.keypadNumber}>5</Text>
+        </View>
+        <View style={styles.keypadButton}>
+          <Text style={styles.keypadNumber}>6</Text>
+        </View>
+      </View>
+      <View style={styles.keypadRow}>
+        <View style={styles.keypadButton}>
+          <Text style={styles.keypadNumber}>7</Text>
+        </View>
+        <View style={styles.keypadButton}>
+          <Text style={styles.keypadNumber}>8</Text>
+        </View>
+        <View style={styles.keypadButton}>
+          <Text style={styles.keypadNumber}>9</Text>
+        </View>
+      </View>
+      <View style={styles.keypadRow}>
+        <View style={styles.keypadButton}>
+          <Text> </Text>
+        </View>
+        <View style={styles.keypadButton}>
+          <Text style={styles.keypadNumber}>0</Text>
+        </View>
+        <View style={styles.keypadButton}>
+          <Text style={styles.keypadDeleteText}>DELETE</Text>
+        </View>
+      </View>
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  keypadContainer: {
+    height: '55%',
+    width: '100%',
+    backgroundColor: '#191919',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+  },
+  keypadRow: {
+    display: 'flex',
+    flexDirection: 'row',
+    height: '25%',
+  },
+  keypadButton: {
+    width: '33%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  keypadNumber: {
+    color: '#fff',
+    fontSize: 30,
+    fontFamily: 'Avenir',
+    fontWeight: '500',
+  },
+  keypadDeleteText: {
+    color: '#fff',
+    fontSize: 16,
+    fontFamily: 'Avenir',
+    fontWeight: '800',
+    letterSpacing: 2,
+  },
+});
 
 export default Keypad;
---
+++ components/Passcode.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const Passcode = () => {
+  return (
+    <View style={styles.passcodeContainer}>
+      <Text style={styles.passcodeText}>ENTER PASSCODE</Text>
+      <View style={styles.passcodeMaskContainer}>
+        <View style={styles.passcodeMaskChar} />
+        <View style={styles.passcodeMaskChar} />
+        <View style={styles.passcodeMaskChar} />
+        <View style={styles.passcodeMaskChar} />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  passcodeContainer: {
+    height: '45%',
+    width: '100%',
+    backgroundColor: '#101010',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  passcodeText: {
+    color: '#fff',
+    fontSize: 20,
+    fontWeight: '800',
+    fontFamily: 'Avenir',
+    letterSpacing: 4,
+  },
+  passcodeMaskContainer: {
+    height: 50,
+    width: 250,
+    display: 'flex',
+    flexDirection: 'row',
+    marginTop: 20,
+  },
+  passcodeMaskChar: {
+    color: '#fff',
+    height: 40,
+    width: 40,
+    marginLeft: 10,
+    marginRight: 10,
+    borderRadius: 20,
+    borderColor: '#fff',
+    borderWidth: 1,
+  },
+});
+
+export default Passcode;
